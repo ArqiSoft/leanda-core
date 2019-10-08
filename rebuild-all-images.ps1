71df92e3f25bc6a2b85b1ad89027ab7d98562f9a
olegkuibar <kuibar.oleg@gmail.com>
2019-10-08 18:04:34 +0300

    feat(LEAN-104): tag all images as 'local'

--- rebuild-all-images.ps1
+++ rebuild-all-images.ps1
@@ -1,9 +1,9 @@
-docker build -t leanda/core-persistence:latest -f Sds.Osdr.Persistence/Dockerfile .
-docker build -t leanda/core-frontend:latest -f Sds.Osdr.Domain.FrontEnd/Dockerfile .
-docker build -t leanda/core-backend:latest -f Sds.Osdr.Domain.BackEnd/Dockerfile .
-docker build -t leanda/core-sagahost:latest -f Sds.Osdr.Domain.SagaHost/Dockerfile .
-docker build -t leanda/core-web-api:latest -f Sds.Osdr.WebApi/Dockerfile .
-docker build -t leanda/integration:latest -f Sds.Osdr.IntegrationTests/Dockerfile .
-docker build -t leanda/webapi-integration:latest -f Sds.Osdr.WebApi.IntegrationTests/Dockerfile .
-docker build -t leanda/e2e-tests:latest -f Sds.Osdr.EndToEndTests/Dockerfile .
+docker build -t leanda/core-persistence:local -f Sds.Osdr.Persistence/Dockerfile .
+docker build -t leanda/core-frontend:local -f Sds.Osdr.Domain.FrontEnd/Dockerfile .
+docker build -t leanda/core-backend:local -f Sds.Osdr.Domain.BackEnd/Dockerfile .
+docker build -t leanda/core-sagahost:local -f Sds.Osdr.Domain.SagaHost/Dockerfile .
+docker build -t leanda/core-web-api:local -f Sds.Osdr.WebApi/Dockerfile .
+docker build -t leanda/integration:local -f Sds.Osdr.IntegrationTests/Dockerfile .
+docker build -t leanda/webapi-integration:local -f Sds.Osdr.WebApi.IntegrationTests/Dockerfile .
+docker build -t leanda/e2e-tests:local -f Sds.Osdr.EndToEndTests/Dockerfile .
 docker image ls leanda/*
